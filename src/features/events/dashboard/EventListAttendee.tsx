698be259f3d3a780d8905638782449da10bab563
vineetson <vineetsonig.2012@gmail.com>
2023-12-28 18:09:05 +0530

    made events, react components, used effects

---
+++ src/features/events/dashboard/EventListAttendee.tsx
@@ -0,0 +1,9 @@
+import { List, Image } from "semantic-ui-react";
+
+export default function EventListAttendee({attendee}: any) {
+  return (
+    <List.Item>
+      <Image size="mini" circular src={attendee.photoURL} />
+    </List.Item>
+  )
+}
